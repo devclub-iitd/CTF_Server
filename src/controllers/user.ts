96503d7876d5c368d39eb07645b521c6a0864d8f
Harsh Agrawal <harsh.ag14901@gmail.com>
2021-04-12 17:52:53 +0530

    Autoextract login/signup credentials from SSO cookie

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -3,7 +3,7 @@ import jwt, { Secret } from "jsonwebtoken";
 import { Request, Response, NextFunction } from "express";
 import User from "../models/user";
 import initCRUD from "../utils/crudFactory";
-import { createResponse, createError } from "../utils/helper";
+import { createResponse, createError, makeid } from "../utils/helper";
 import { validateToken } from '../utils/tokenValidator';
 
 const router = express.Router({mergeParams: true});
@@ -11,19 +11,8 @@ const [create, get, update, all] = initCRUD(User);
 
 const userLogin = (req:Request, res: Response, next:NextFunction) => {
     let p_username : string;
-    let p_password : string;
-
-    if (!req.body) {
-        const result = {
-            message: 'Received request with no body',
-            status: 404
-        }
-        return res.status(200).json(result)
-        //next(createError(400, "Bad request", "Received request with no body"));
-    }
-
-    p_password = req.body.password;
-    p_username = req.body.username;
+   
+    p_username = req.user.username;
     
     // Retrieve the username from the database
     User.findOne({username: p_username}).then((userDoc) => {
@@ -37,45 +26,32 @@ const userLogin = (req:Request, res: Response, next:NextFunction) => {
         } else {
             let userObject = userDoc.toObject();
 
-            // Get the password
-            let userPassword = userObject.password;
+            const payload = {user: userObject.username};
+            const options = {expiresIn: '2d', issuer: 'devclub-ctf'};
+            //const secret = process.env.JWT_SECRET as Secret;
+            const secret = 'secret' as Secret
             
-            if (userPassword == p_password) {
-                // Create a token
-                const payload = {user: userObject.username};
-                const options = {expiresIn: '2d', issuer: 'devclub-ctf'};
-                //const secret = process.env.JWT_SECRET as Secret;
-                const secret = 'secret' as Secret
-                
-                if (secret === undefined) {
-                    const result = {
-                        message: `Token secret key not initialized`,
-                        status: 500
-                    }
-                    return res.status(200).json(result)
-                    //next(createError(500, "Incorrect configuration", `Token secret key not initialized`));
-                }
-
-                
-                const token = jwt.sign(payload, secret, options);
-
-                let result = {
-                    message: 'Successfull Login',
-                    status: 200,
-                    token: token,
-                    userId: userObject._id,
-                    expiresIn: 172800 as Number
-                };
-
-                return res.status(200).send(result);
-            } else {
+            if (secret === undefined) {
                 const result = {
-                    message: `Password is Incorrect!!`,
-                    status: 400
+                    message: `Token secret key not initialized`,
+                    status: 500
                 }
                 return res.status(200).json(result)
-                //next(createError(400, "Incorrect login", `User with username ${p_username} does not exist or incorrect password entered`));
+                //next(createError(500, "Incorrect configuration", `Token secret key not initialized`));
             }
+
+            
+            const token = jwt.sign(payload, secret, options);
+
+            let result = {
+                message: 'Successfull Login',
+                status: 200,
+                token: token,
+                userId: userObject._id,
+                expiresIn: 172800 as Number
+            };
+
+            return res.status(200).send(result);
         }
     })
     .catch((err) => {
@@ -84,10 +60,11 @@ const userLogin = (req:Request, res: Response, next:NextFunction) => {
 }
 
 const userSignUp : RequestHandler = async (req, res, next) => {
+    console.log(req.user);
     const userBody = {
-        username: req.body.username,
-        password: req.body.password,
-        email: req.body.email
+        username: req.user.username,
+        password: makeid(16),
+        email: req.user.email
     }
     const user = new User(userBody)
     try{
--- src/utils/helper.ts
+++ src/utils/helper.ts
@@ -10,3 +10,17 @@ export const createError = (status: number, name: string, message: string) => {
   e.message = message;
   return e;
 };
+
+export const makeid = (length: number, alpahnum_only : boolean = false) => {
+  let result = '';
+  const characters = !alpahnum_only
+      ? 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789,./;:?><[]{}|`~!@#$%^&*()-_=+'
+      : 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  const charactersLength = characters.length;
+  for (let i = 0; i < length; i += 1) {
+      result += characters.charAt(
+          Math.floor(Math.random() * charactersLength)
+      );
+  }
+  return result;
+};
